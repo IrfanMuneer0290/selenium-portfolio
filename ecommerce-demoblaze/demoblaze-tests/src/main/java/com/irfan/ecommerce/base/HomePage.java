bc9d5e1d40b846587ffc6f87cad6ef006336903f
Irfan Muneer <Irfaanpashamuunir@gmail.com>
2026-02-15 22:26:34 +0400

    updated splun to run in docker container

--- ecommerce-demoblaze/demoblaze-tests/src/main/java/com/irfan/ecommerce/base/HomePage.java
+++ ecommerce-demoblaze/demoblaze-tests/src/main/java/com/irfan/ecommerce/base/HomePage.java
@@ -1,5 +1,7 @@
 package com.irfan.ecommerce.base;
 import java.time.Duration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,6 +11,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
+     private static final Logger logger = LogManager.getLogger(HomePage.class);
     private WebDriver driver = DriverFactory.getDriver();
     private final String URL = "https://www.demoblaze.com/";
     
@@ -33,6 +36,7 @@ public class HomePage {
     }
 
     public String getTitleText() {
+         logger.info("SPLUNK TEST: Home Page getTitleText!");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOf(storeTitle));
         return storeTitle.getText();
--- ecommerce-demoblaze/demoblaze-tests/src/test/java/com/irfan/ecommerce/tests/HomePageTest.java
+++ ecommerce-demoblaze/demoblaze-tests/src/test/java/com/irfan/ecommerce/tests/HomePageTest.java
@@ -1,5 +1,7 @@
 package com.irfan.ecommerce.tests;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -7,8 +9,11 @@ import com.irfan.ecommerce.base.BaseTest;
 import com.irfan.ecommerce.base.HomePage;
 
 public class HomePageTest extends BaseTest {
+
+     private static final Logger logger = LogManager.getLogger(HomePageTest.class);
     @Test
     public void testHomePageTitle() {
+         logger.info("SPLUNK TEST: Home Page Test is running!");
         HomePage homePage = new HomePage(driver); 
         homePage.open();
         String actualTitle = homePage.getTitleText();
