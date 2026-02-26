ca11744a3831018d41c183730862e3aedb2f951f
Irfan Muneer <Irfaanpashamuunir@gmail.com>
2026-02-27 00:03:40 +0400

    fix: implement headless browser configuration for CI/CD linux runners

--- ecommerce-demoblaze/demoblaze-tests/src/main/java/com/irfan/ecommerce/ui/base/DriverFactory.java
+++ ecommerce-demoblaze/demoblaze-tests/src/main/java/com/irfan/ecommerce/ui/base/DriverFactory.java
@@ -56,6 +56,8 @@ public class DriverFactory {
         options.addArguments("--headless=new");
         options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
     }
 
     try {
